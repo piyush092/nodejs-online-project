f004ade215b92f25ce5789935e72ef362d0bd10f
Abhisheks <piyushagrawal092@gmail.com>
2022-06-22 11:12:42 +0530

    Update Labour_report.js

--- Sheet/Labour_Report/Labour_report.js
+++ Sheet/Labour_Report/Labour_report.js
@@ -27,7 +27,7 @@ module.exports =  (connection,req)=>
          var q_4 = `SELECT SUM(unloading) as unloadingsum,SUM(diversion) as dversion,entryDate as entryDate,depot_code as de
          from outtable where entryDate between '${req.body['Start_Date']}' AND '${req.body['End_Date']}'
          and depot_code='${req.body['Depot_Code'] }' and deleteflag='0' and grade='DSP' GROUP BY entryDate;`;  
-            connection.query(q_1+''+q_2+''+q_3+''+q_4, [1,2,3,4], (e, r) =>
+            connection.query(q_1+q_2+q_3+q_4, [1,2,3,4], (e, r) =>
             {
                 if (e){
                     resolve({ status: false, message: 'Somethings wrong...', error: e });
