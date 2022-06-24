f65441a25d1af81a04dc144b219350f618b46809
Abhisheks <piyushagrawal092@gmail.com>
2022-06-24 16:55:25 +0530

    Update EmplyoeesInsert.js

--- Sheet/Admin/EmplyoeesInsert.js
+++ Sheet/Admin/EmplyoeesInsert.js
@@ -13,7 +13,7 @@ function  Insert (CONNECTION, DATA,res)
         var INSERT_QUERY_INWARD_SHEET = `INSERT INTO employee_details 
         VALUES ('${ DATA['Emp_Id'] }','${ DATA['Employees_Name'] }',
         '${ DATA['Email_Id'] }','${ DATA['Password'] }',
-        '${ DATA['Confirm_Password'] }','${ DATA['Contact_Number'] }',
+        '${ DATA['Contact_Number'] }',
         '${ DATA['Depot_Code'] }','00:00:00','${ DATA['Role'] }');`;
   
         CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
