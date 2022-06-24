51e9702fcfac75878d1203662b5c20a937342f45
Abhisheks <piyushagrawal092@gmail.com>
2022-06-24 17:17:43 +0530

    Update EmplyoeesInsert.js

--- Sheet/Admin/EmplyoeesInsert.js
+++ Sheet/Admin/EmplyoeesInsert.js
@@ -14,22 +14,34 @@ function  Insert (CONNECTION, DATA,res)
     {
         if (DATA['DEPOT_NAME'] != '' && DATA['DEPOT_CODE'] != '')
     {
-        var INSERT_QUERY_INWARD_SHEET = `INSERT INTO employee_details 
-        VALUES ('${ DATA['Emp_Id'] }','${ DATA['Employees_Name'] }',
-        '${ DATA['Email_Id'] }','${ DATA['Password'] }',
-        '${ DATA['Contact_Number'] }',
-        '${ DATA['Depot_Code'] }','00:00:00','${ DATA['Role'] }');`;
-  
-       await CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
+        await CONNECTION.query(`Select * from employee_details where emp_id='${DATA['Emp_Id']}'`, [1],async function(err, results) {
             if (err){
-                resolve({ Status: true, Error:err,Result:[results]});
+                  resolve({ Status: true, Error:err,Result:[]});
             }
-          if (results['affectedRows']!=0) {
-               resolve({ Status: true, Message: 'Insert Successfully',Result:[results]});
-          } else{
-               resolve({ Status: true, Error:err,Result:[results]});
-          }
-        });   
+            console.log(results);
+            if (results['affectedRows'] != 0)
+            {
+                var INSERT_QUERY_INWARD_SHEET = `INSERT INTO employee_details 
+                VALUES ('${ DATA['Emp_Id'] }','${ DATA['Employees_Name'] }',
+                '${ DATA['Email_Id'] }','${ DATA['Password'] }',
+                '${ DATA['Contact_Number'] }',
+                '${ DATA['Depot_Code'] }','00:00:00','${ DATA['Role'] }');`;
+                await CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
+                    if (err){
+                          resolve({ Status: true, Error:err,Result:[]});
+                      }
+                    if (results['affectedRows']!=0) {
+                         resolve({ Status: true, Message: 'Insert Successfully',Result:[results]});
+                    } else{
+                         resolve({ Status: true, Error:err,Result:[results]});
+                    }
+                  });  
+                 resolve({ Status: true, Message: 'Insert Successfully',Result:[results]});
+            } else{
+                 resolve({ Status: true, Error:err,Result:[results]});
+            }
+          });  
+      
     } else
     {
         resolve({ Status: true, Error:'Please Check any Input Empty',Result:[]});   
