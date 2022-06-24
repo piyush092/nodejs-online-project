49602e5c628c8ea640bde14eb87e1d4c0cdb4e1f
Abhisheks <piyushagrawal092@gmail.com>
2022-06-24 17:12:20 +0530

    Update EmplyoeesInsert.js

--- Sheet/Admin/EmplyoeesInsert.js
+++ Sheet/Admin/EmplyoeesInsert.js
@@ -2,13 +2,17 @@ module.exports = function (app, MYSQL_CON)
 {
     app.post('/Admin/EmplyoesInsert', (req,res) =>
     {
-        Insert(MYSQL_CON, req.body,res);
+        Insert(MYSQL_CON, req.body, res).then((r) =>{
+            res.json(r);
+        });
     });
 }
 
 function  Insert (CONNECTION, DATA,res)
 {
-    if (DATA['DEPOT_NAME'] != '' && DATA['DEPOT_CODE'] != '')
+    return new Promise(async (resolve,reject) =>
+    {
+        if (DATA['DEPOT_NAME'] != '' && DATA['DEPOT_CODE'] != '')
     {
         var INSERT_QUERY_INWARD_SHEET = `INSERT INTO employee_details 
         VALUES ('${ DATA['Emp_Id'] }','${ DATA['Employees_Name'] }',
@@ -16,18 +20,19 @@ function  Insert (CONNECTION, DATA,res)
         '${ DATA['Contact_Number'] }',
         '${ DATA['Depot_Code'] }','00:00:00','${ DATA['Role'] }');`;
   
-        CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
+       await CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
             if (err){
-                res.json({ Status: true, Error:err,Result:[results]});
+                resolve({ Status: true, Error:err,Result:[results]});
             }
           if (results['affectedRows']!=0) {
-               res.json({ Status: true, Message: 'Insert Successfully',Result:[results]});
+               resolve({ Status: true, Message: 'Insert Successfully',Result:[results]});
           } else{
-               res.json({ Status: true, Error:err,Result:[results]});
+               resolve({ Status: true, Error:err,Result:[results]});
           }
         });   
     } else
     {
-        res.json({ Status: true, Error:'Please Check any Input Empty',Result:[]});   
+        resolve({ Status: true, Error:'Please Check any Input Empty',Result:[]});   
     }
+    });
 }
