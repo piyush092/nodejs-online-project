0431f0504a6a7dcd3450573131c23c0f8a2c0f38
Abhisheks <piyushagrawal092@gmail.com>
2022-06-24 16:43:09 +0530

    Admin Page

---
+++ Sheet/Admin/DepotInsert.js
@@ -0,0 +1,27 @@
+module.exports = function (app, MYSQL_CON)
+{
+    app.post('/Admin/DepotInsert', (req,res) =>
+    {
+        Insert(MYSQL_CON, req.body,res);
+    });
+}
+
+function  Insert (CONNECTION, DATA,res)
+{
+    if (DATA['DEPOT_NAME'] != '' && DATA['DEPOT_CODE'] != '')
+    {
+        var INSERT_QUERY_INWARD_SHEET = `INSERT INTO depot (depot_Name,depot_code) VALUES ('${DATA['DEPOT_NAME']}','${DATA['DEPOT_CODE']}');`;
+  
+        CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
+            if (err) throw err;
+          if (results['affectedRows']!=0) {
+               res.json({ Status: true, Message: 'Insert Successfully',Result:[results]});
+          } else{
+               res.json({ Status: true, Error:err,Result:[results]});
+          }
+        });   
+    } else
+    {
+        res.json({ Status: true, Error:'Please Check any Input Empty',Result:[]});   
+    }
+}
---
+++ Sheet/Admin/EmplyoeesInsert.js
@@ -0,0 +1,31 @@
+module.exports = function (app, MYSQL_CON)
+{
+    app.post('/Admin/EmplyoesInsert', (req,res) =>
+    {
+        Insert(MYSQL_CON, req.body,res);
+    });
+}
+
+function  Insert (CONNECTION, DATA,res)
+{
+    if (DATA['DEPOT_NAME'] != '' && DATA['DEPOT_CODE'] != '')
+    {
+        var INSERT_QUERY_INWARD_SHEET = `INSERT INTO employee_details 
+        VALUES ('${ DATA['EMP_ID'] }','${ DATA['Employees_Name'] }',
+        '${ DATA['Email_Id'] }','${ DATA['Password'] }',
+        '${ DATA['Confirm_Password'] }','${ DATA['Contact_Number'] }',
+        '${ DATA['Depot_Code'] }','00:00:00','${ DATA['Role'] }');`;
+  
+        CONNECTION.query(INSERT_QUERY_INWARD_SHEET, [1], function(err, results) {
+            if (err) throw err;
+          if (results['affectedRows']!=0) {
+               res.json({ Status: true, Message: 'Insert Successfully',Result:[results]});
+          } else{
+               res.json({ Status: true, Error:err,Result:[results]});
+          }
+        });   
+    } else
+    {
+        res.json({ Status: true, Error:'Please Check any Input Empty',Result:[]});   
+    }
+}
